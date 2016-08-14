ce2feeeb0ccb24bc9263c2d69d50e3ffd9d319b6
Markus Kobler <markus.kobler@digital.hmrc.gov.uk>
2016-08-14 19:53:28 +0100

    Enabling plugin

--- build.sh
+++ build.sh
@@ -1,37 +1,43 @@
-#!/usr/bin/env sh
+#!/bin/sh
 set -eux
 
 ELASTICSEARCH_VERSION=2.3.1
+KOPF_VERSION=2.1.2
 
 SRC=$PWD/src
-OUT=$PWD/elasticsearch-build
+OUT=$PWD/out
 ROOTFS=$PWD/rootfs
 
 mkdir -p $OUT/elasticsearch $OUT/tmp
-curl -OL https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/$ELASTICSEARCH_VERSION/elasticsearch-$ELASTICSEARCH_VERSION.tar.gz
+curl -sOL https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/$ELASTICSEARCH_VERSION/elasticsearch-$ELASTICSEARCH_VERSION.tar.gz
 # shasum check
 tar -C $OUT/elasticsearch --strip-components 1 -xf elasticsearch-$ELASTICSEARCH_VERSION.tar.gz
 
-# TODO add Plugins
+mkdir -p $OUT/elasticsearch/tmp $OUT/elasticsearch/plugins
+chmod 1777 $OUT/elasticsearch/tmp
 
-mkdir -p $OUT/elasticsearch/tmp
+mkdir -p $OUT/elasticsearch/plugins/kopf
+curl -sL https://github.com/lmenezes/elasticsearch-kopf/archive/v$KOPF_VERSION.tar.gz -o kopf-v$KOPF_VERSION.tar.gz
+tar -C $OUT/elasticsearch/plugins/kopf --strip-components 1 -xf kopf-v$KOPF_VERSION.tar.gz
 
 #mkdir -p $OUT/elasticsearch/data $OUT/elasticsearch/work $OUT/elasticsearch/logs $OUT/elasticsearch/plugins
-chown -R nobody:root $OUT/elasticsearch
 
 cat <<EOF > $OUT/Dockerfile
 FROM quay.io/desource/java
 
-ADD elasticsearch     /elasticsearch
+ADD elasticsearch /elasticsearch
 
-USER nobody
+# USER nobody
 
 VOLUME ["/elasticsearch/data"]
 
-ENTRYPOINT ["java", "-cp", "/elasticsearch/lib/elasticsearch-${ELASTICSEARCH_VERSION}.jar:/elasticsearch/lib/*", "-Djava.awt.headless=true", "-XX:+UseParNewGC", "-XX:+UseConcMarkSweepGC", "-XX:CMSInitiatingOccupancyFraction=75", "-XX:+UseCMSInitiatingOccupancyOnly", "-XX:+HeapDumpOnOutOfMemoryError", "-XX:+DisableExplicitGC", "-Dfile.encoding=UTF-8", "-Delasticsearch", "-Des.foreground=yes", "-Des.path.home=/elasticsearch", "-Djna.tmpdir=/elasticsearch/tmp"]
+ENTRYPOINT ["java", "-cp", "/elasticsearch/lib/elasticsearch-${ELASTICSEARCH_VERSION}.jar:/elasticsearch/lib/*", "-Djava.awt.headless=true", "-XX:+UseParNewGC", "-XX:+UseConcMarkSweepGC", "-XX:CMSInitiatingOccupancyFraction=75", "-XX:+UseCMSInitiatingOccupancyOnly", "-XX:+HeapDumpOnOutOfMemoryError", "-XX:+DisableExplicitGC", "-Dfile.encoding=UTF-8", "-Djna.tmpdir=/elasticsearch/tmp", "-Delasticsearch", "-Des.foreground=yes", "-Des.path.home=/elasticsearch", "-Des.insecure.allow.root=true"]
 
 CMD ["-Des.discovery.zen.ping.multicast.enabled=false", "-Xms256m", "-Xmx1g", "org.elasticsearch.bootstrap.Elasticsearch", "start"]
 
 EXPOSE 9200 9300
 
 EOF
+
+# TODO sort   "-Des.bootstrap.mlockall=true"
+# TODO remove "-Des.insecure.allow.root=true"

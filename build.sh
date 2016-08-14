db4ae7ccd852377a7897a2368ad86b2f9b96bcac
Markus Kobler <markus.kobler@digital.hmrc.gov.uk>
2016-08-14 18:58:53 +0100

    Meh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ SRC=$PWD/src
 OUT=$PWD/elasticsearch-build
 ROOTFS=$PWD/rootfs
 
-mkdir -p $OUT/elasticsearch
+mkdir -p $OUT/elasticsearch $OUT/tmp
 curl -OL https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/$ELASTICSEARCH_VERSION/elasticsearch-$ELASTICSEARCH_VERSION.tar.gz
 # shasum check
 tar -C $OUT/elasticsearch --strip-components 1 -xf elasticsearch-$ELASTICSEARCH_VERSION.tar.gz
@@ -16,13 +16,14 @@ tar -C $OUT/elasticsearch --strip-components 1 -xf elasticsearch-$ELASTICSEARCH_
 
 cp -r $SRC/etc $OUT
 
-mkdir -p $OUT/elasticsearch/data $OUT/elasticsearch/work $OUT/elasticsearch/logs $OUT/elasticsearch/plugins
-chown -R nobody $OUT/elasticsearch/{config,data,work,logs,plugins}
+#mkdir -p $OUT/elasticsearch/data $OUT/elasticsearch/work $OUT/elasticsearch/logs $OUT/elasticsearch/plugins
+chown -R nobody $OUT/elasticsearch
 
 cat <<EOF > $OUT/Dockerfile
 FROM quay.io/desource/java
 
 ADD ./etc               /etc
+ADD ./tmp               /tmp
 ADD ./elasticsearch     /elasticsearch
 
 EXPOSE 9200 9300

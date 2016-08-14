28d669510a17f5f5786123aa7a7410699c645968
Markus Kobler <markus.kobler@digital.hmrc.gov.uk>
2016-08-14 17:50:11 +0100

    Tidying up permissions

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@ set -eux
 
 ELASTICSEARCH_VERSION=2.3.1
 
-BASE=$PWD
 SRC=$PWD/src
 OUT=$PWD/elasticsearch-build
 ROOTFS=$PWD/rootfs
@@ -12,20 +11,28 @@ mkdir -p $OUT/elasticsearch
 curl -OL https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/$ELASTICSEARCH_VERSION/elasticsearch-$ELASTICSEARCH_VERSION.tar.gz
 tar -C $OUT/elasticsearch --strip-components 1 -xf elasticsearch-$ELASTICSEARCH_VERSION.tar.gz
 
+# TODO add Plugins
+
 cp -r $SRC/etc $OUT
+
+mkdir -p $OUT/data/data $OUT/data/work $OUT/data/log
+chown -R nobody $OUT/data
 
 cat <<EOF > $OUT/Dockerfile
 FROM quay.io/desource/java
 
 ADD ./elasticsearch     /elasticsearch
 ADD ./etc               /etc
+ADD ./data              /data
 
 EXPOSE 9200 9300
 
-VOLUME /data/data /data/log /data/plugins
+USER nobody
 
-ENTRYPOINT ["java", "-cp", "/elasticsearch/lib/elasticsearch-${ELASTICSEARCH_VERSION}.jar:/elasticsearch/lib/*", "-Djava.awt.headless=true", "-XX:+UseParNewGC", "-XX:+UseConcMarkSweepGC", "-XX:CMSInitiatingOccupancyFraction=75", "-XX:+UseCMSInitiatingOccupancyOnly", "-XX:+HeapDumpOnOutOfMemoryError", "-XX:+DisableExplicitGC", "-Dfile.encoding=UTF-8", "-Delasticsearch", "-Des.foreground=yes", "-Des.path.home=/elasticsearch/", "-Des.insecure.allow.root=true"]
+VOLUME ["/data/data", "/data/work", "/data/log"]
 
-CMD [ "-Des.insecure.allow.root=true", "-Xms256m", "-Xmx1g", "org.elasticsearch.bootstrap.Elasticsearch", "start" ]
+ENTRYPOINT ["java", "-cp", "/elasticsearch/lib/elasticsearch-${ELASTICSEARCH_VERSION}.jar:/elasticsearch/lib/*", "-Djava.awt.headless=true", "-XX:+UseParNewGC", "-XX:+UseConcMarkSweepGC", "-XX:CMSInitiatingOccupancyFraction=75", "-XX:+UseCMSInitiatingOccupancyOnly", "-XX:+HeapDumpOnOutOfMemoryError", "-XX:+DisableExplicitGC", "-Dfile.encoding=UTF-8", "-Des.foreground=yes", "-Des.path.home=/elasticsearch"]
+
+CMD ["-Xms256m", "-Xmx1g", "org.elasticsearch.bootstrap.Elasticsearch", "start"]
 
 EOF

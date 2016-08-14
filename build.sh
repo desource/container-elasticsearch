542e8c3e43e3b841632f45055c817d6f358eeefa
Markus Kobler <markus.kobler@digital.hmrc.gov.uk>
2016-08-14 18:15:57 +0100

    Fixing directory

--- build.sh
+++ build.sh
@@ -9,27 +9,27 @@ ROOTFS=$PWD/rootfs
 
 mkdir -p $OUT/elasticsearch
 curl -OL https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/$ELASTICSEARCH_VERSION/elasticsearch-$ELASTICSEARCH_VERSION.tar.gz
+# shasum check
 tar -C $OUT/elasticsearch --strip-components 1 -xf elasticsearch-$ELASTICSEARCH_VERSION.tar.gz
 
 # TODO add Plugins
 
 cp -r $SRC/etc $OUT
 
-mkdir -p $OUT/data/data $OUT/data/work $OUT/data/log
-chown -R nobody $OUT/data
+mkdir -p $OUT/elasticsearch/data $OUT/elasticsearch/work $OUT/elasticsearch/log $OUT/elasticsearch/plugins
+chown -R nobody $OUT/elasticsearch/data $OUT/elasticsearch/work $OUT/elasticsearch/log $OUT/elasticsearch/plugins
 
 cat <<EOF > $OUT/Dockerfile
 FROM quay.io/desource/java
 
 ADD ./elasticsearch     /elasticsearch
 ADD ./etc               /etc
-ADD ./data              /data
 
 EXPOSE 9200 9300
 
 USER nobody
 
-VOLUME ["/data/data", "/data/work", "/data/log"]
+VOLUME ["/elasticsearch/data", "/elasticsearch/work", "/elasticsearch/log"]
 
 ENTRYPOINT ["java", "-cp", "/elasticsearch/lib/elasticsearch-${ELASTICSEARCH_VERSION}.jar:/elasticsearch/lib/*", "-Djava.awt.headless=true", "-XX:+UseParNewGC", "-XX:+UseConcMarkSweepGC", "-XX:CMSInitiatingOccupancyFraction=75", "-XX:+UseCMSInitiatingOccupancyOnly", "-XX:+HeapDumpOnOutOfMemoryError", "-XX:+DisableExplicitGC", "-Dfile.encoding=UTF-8", "-Des.foreground=yes", "-Des.path.home=/elasticsearch"]
 

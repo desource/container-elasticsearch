148384bdb86417b86331a3fffae577fc740392b0
Markus Kobler <markus.kobler@digital.hmrc.gov.uk>
2016-08-14 19:18:15 +0100

    Trying to fix

--- build.sh
+++ build.sh
@@ -14,19 +14,30 @@ tar -C $OUT/elasticsearch --strip-components 1 -xf elasticsearch-$ELASTICSEARCH_
 
 # TODO add Plugins
 
-cp -r $SRC/etc $OUT
+mkdir -p $OUT/tmp $OUT/etc
+
+cat <<EOF > $OUT/etc/passwd
+root:x:0:0:root:/:/dev/null
+nobody:x:65534:65534:nogroup:/:/dev/null
+EOF
+
+cat <<EOF > $OUT/etc/group
+root:x:0:
+nogroup:x:65534:
+EOF
+
+chmod 1777 $OUT/tmp
 
 #mkdir -p $OUT/elasticsearch/data $OUT/elasticsearch/work $OUT/elasticsearch/logs $OUT/elasticsearch/plugins
-chown -R nobody $OUT/elasticsearch
+chown -R nobody:root $OUT/elasticsearch
 
 cat <<EOF > $OUT/Dockerfile
 FROM quay.io/desource/java
 
-ADD ./etc               /etc
-ADD ./tmp               /tmp
-ADD ./elasticsearch     /elasticsearch
-
-EXPOSE 9200 9300
+ADD etc/passwd        /etc/
+ADD etc/group         /etc/
+ADD tmp               /tmp
+ADD elasticsearch     /elasticsearch
 
 USER nobody
 
@@ -35,5 +46,7 @@ VOLUME ["/elasticsearch/data"]
 ENTRYPOINT ["java", "-cp", "/elasticsearch/lib/elasticsearch-${ELASTICSEARCH_VERSION}.jar:/elasticsearch/lib/*", "-Djava.awt.headless=true", "-XX:+UseParNewGC", "-XX:+UseConcMarkSweepGC", "-XX:CMSInitiatingOccupancyFraction=75", "-XX:+UseCMSInitiatingOccupancyOnly", "-XX:+HeapDumpOnOutOfMemoryError", "-XX:+DisableExplicitGC", "-Dfile.encoding=UTF-8", "-Delasticsearch", "-Des.foreground=yes", "-Des.path.home=/elasticsearch"]
 
 CMD ["-Des.discovery.zen.ping.multicast.enabled=false", "-Xms256m", "-Xmx1g", "org.elasticsearch.bootstrap.Elasticsearch", "start"]
+
+EXPOSE 9200 9300
 
 EOF
